81814ce457014759417071423d05fadfcac657a6
Estevo <>
2017-02-24 13:49:56 -0300

    Formulario ajax com validações js usando webservice

--- form_ajax/index.php
+++ form_ajax/index.php
@@ -14,7 +14,7 @@ include "./source/conexao.php";
         <!-- Dependência jQuery-->
         <script   src="https://code.jquery.com/jquery-1.12.4.min.js"   integrity="sha256-ZosEbRLbNQzLpnKIkEdrPv7lOy9C27hHQ+Xp8a4MxAQ="   crossorigin="anonymous"></script>
         <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
-        <!--mascaras -->
+        <!--mascaras  -->
         <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery.maskedinput/1.4.1/jquery.maskedinput.js" integrity="sha256-fvFKHgcKai7J/0TM9ekjyypGDFhho9uKmuHiFVfScCA=" crossorigin="anonymous"></script>
         <title>Cadastro</title>
         <script>
